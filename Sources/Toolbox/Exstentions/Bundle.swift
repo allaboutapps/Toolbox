7eab2fcebd106b4ca948712383fd109898a99309
Sandin <sandin.dulic@allaboutapps.at>
2020-09-07 15:22:07 +0200

    add stringToInt

--- Sources/Toolbox/Exstentions/Bundle.swift
+++ Sources/Toolbox/Exstentions/Bundle.swift
@@ -3,8 +3,18 @@ import Foundation
 
 private class BundleClass {}
 
-extension Bundle {
-    public static var current: Bundle {
+public extension Bundle {
+    static var current: Bundle {
         return Bundle(for: BundleClass.self)
+    }
+}
+
+public extension Bundle {
+    var appVersion: String? {
+        self.infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    
+    static var mainAppVersion: String? {
+        Bundle.main.appVersion
     }
 }
