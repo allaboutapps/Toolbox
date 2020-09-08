bbb938bb299fea5ce904cae5a3803d334dfa971c
Sandin <sandin.dulic@allaboutapps.at>
2020-09-08 11:31:02 +0200

    add public to exstention

--- Sources/Toolbox/Exstentions/SwiftUI/View+HideKeyboard.swift
+++ Sources/Toolbox/Exstentions/SwiftUI/View+HideKeyboard.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 #if canImport(UIKit)
 @available(iOS 13.0, *)
-extension View {
+public extension View {
     func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
