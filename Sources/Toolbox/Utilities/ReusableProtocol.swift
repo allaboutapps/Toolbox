8b096861cffbe0ef4f1d01671d4b5006a9016948
Sandin <sandin.dulic@allaboutapps.at>
2020-09-09 09:32:06 +0200

    add reusable protocol for uicollection view

---
+++ Sources/Toolbox/Utilities/ReusableProtocol.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public protocol Reusable: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
+public extension Reusable {
+    static var reuseIdentifier: String { return String(describing: self) }
+}
+
+extension UICollectionViewCell: Reusable {}
