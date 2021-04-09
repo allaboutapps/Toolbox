b4fcef03cb7e5b2e609e6a3dc34c22866ffa4c1d
Matthias Buchetics <matthias.buchetics@allaboutapps.at>
2021-04-09 14:44:43 +0200

    added SwiftUI programmatic navigation extensions

---
+++ Sources/Toolbox/Extensions/SwiftUI/View+Navigation.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+// see https://swiftwithmajid.com/2021/01/27/lazy-navigation-in-swiftui/
+
+@available(iOS 13.0, *)
+extension NavigationLink where Label == EmptyView {
+    init?<Value>(_ binding: Binding<Value?>, @ViewBuilder destination: (Value) -> Destination) {
+        guard let value = binding.wrappedValue else {
+            return nil
+        }
+
+        let isActive = Binding(
+            get: { true },
+            set: { newValue in if !newValue { binding.wrappedValue = nil } }
+        )
+
+        self.init(destination: destination(value), isActive: isActive, label: EmptyView.init)
+    }
+}
+
+@available(iOS 13.0, *)
+extension View {
+    @ViewBuilder
+    func navigate<Value, Destination: View>(using binding: Binding<Value?>, @ViewBuilder destination: (Value) -> Destination) -> some View {
+        background(NavigationLink(binding, destination: destination))
+    }
+}
