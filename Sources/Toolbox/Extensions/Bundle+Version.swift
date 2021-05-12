d713dd9b70357968af23922e3a390eff5138a99e
Matthias Buchetics <matthias.buchetics@allaboutapps.at>
2021-05-12 09:47:08 +0200

    added build number extensions

--- Sources/Toolbox/Extensions/Bundle+Version.swift
+++ Sources/Toolbox/Extensions/Bundle+Version.swift
@@ -6,11 +6,23 @@ public extension Bundle {
         self.infoDictionary?["CFBundleShortVersionString"] as? String
     }
     
+    var buildNumber: String? {
+        self.infoDictionary?["CFBundleVersion"] as? String
+    }
+    
     var semanticAppVersion: SemanticVersion? {
         appVersion.flatMap { SemanticVersion($0) }
     }
 
     static var mainAppVersion: String? {
         Bundle.main.appVersion
+    }
+    
+    static var mainAppBuildNumber: String? {
+        Bundle.main.buildNumber
+    }
+    
+    static var mainAppVersionWithBuildNumber: String {
+        "\(mainAppVersion ?? "-") (\(mainAppBuildNumber ?? "-")"
     }
 }
