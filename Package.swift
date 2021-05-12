6bb4607a7bc6cc22a38e4fabd18fa2321c55ff00
Matthias Buchetics <matthias.buchetics@allaboutapps.at>
2021-05-12 09:50:42 +0200

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "Toolbox",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
