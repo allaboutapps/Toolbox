26e0a73aa60cfeff5f007a2c420babd8c4d318a2
Mathias Poimer <poimerm1@gmail.com>
2023-08-07 09:22:31 +0200

    bumped min macOS to 11, fixed tests

--- Tests/ToolboxTests/NavigationCoordinatorTests.swift
+++ Tests/ToolboxTests/NavigationCoordinatorTests.swift
@@ -1,25 +1,9 @@
+#if canImport(UIKit)
+
 @testable import Toolbox
 import XCTest
 
 final class NavigationCoordinatorTests: XCTestCase {
-
-    func testNavigationCoordinatorInitialization() async throws {
-        let expectation = XCTestExpectation(description: "NavigationCoordinator initialization")
-        
-        // Switch to the main actor context.
-        await MainActor.run {
-            let navCoordinator = NavigationCoordinator(navigationController: .init())
-            
-            // Test your coordinator instance here, e.g.:
-            XCTAssertNotNil(navCoordinator)
-            
-            // Fulfill the expectation when the test is complete.
-            expectation.fulfill()
-        }
-        
-        // Wait for the expectation to be fulfilled.
-        await fulfillment(of: [expectation], timeout: 10)
-    }
     
     func testNavigationCoordinatorRootIsUINavigationController() async throws {
         let expectation = XCTestExpectation(description: "NavigationCoordinator initialization")
@@ -43,3 +27,5 @@ final class NavigationCoordinatorTests: XCTestCase {
     }
 
 }
+
+#endif
--- Tests/ToolboxTests/TabBarCoordinatorTests.swift
+++ Tests/ToolboxTests/TabBarCoordinatorTests.swift
@@ -1,25 +1,9 @@
+#if canImport(UIKit)
+
 @testable import Toolbox
 import XCTest
 
 final class TabBarCoordinatorTests: XCTestCase {
-
-    func testTabBarCoordinatorInitialization() async throws {
-        let expectation = XCTestExpectation(description: "TabBarCoordinator initialization")
-        
-        // Switch to the main actor context.
-        await MainActor.run {
-            let mainCoordinator = TabBarCoordinator()
-            
-            // Test your coordinator instance here, e.g.:
-            XCTAssertNotNil(mainCoordinator)
-            
-            // Fulfill the expectation when the test is complete.
-            expectation.fulfill()
-        }
-        
-        // Wait for the expectation to be fulfilled.
-        await fulfillment(of: [expectation], timeout: 10)
-    }
     
     func testTabBarCoordinatorRootIsUITabBarController() async throws {
         let expectation = XCTestExpectation(description: "TabBarCoordinator initialization")
@@ -42,3 +26,5 @@ final class TabBarCoordinatorTests: XCTestCase {
         await fulfillment(of: [expectation], timeout: 10)
     }
 }
+
+#endif
